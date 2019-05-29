b0e66dcb07d5e57372dd4a3cbd40a557888eee35
pietto <Pieterjan.v.dijk@gmail.com>
2019-05-29 12:11:33 +0200

    |

---
+++ w24-25-templating/include/carousel.php
@@ -0,0 +1,39 @@
+<div id="myCarousel" class="carousel slide" data-ride="carousel">
+    <ol class="carousel-indicators">
+        <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+        <li data-target="#myCarousel" data-slide-to="1"></li>
+        <li data-target="#myCarousel" data-slide-to="2"></li>
+        <li data-target="#myCarousel" data-slide-to="3"></li>
+    </ol>
+
+    <div class="carousel-inner">
+        <div class="item active">
+            <img src="images/afbeelding1.jpg" alt="carousel image 1">
+        </div>
+
+        <div class="item">
+            <img src="images/afbeelding2.jpg" alt="carousel image 2">
+        </div>
+
+        <div class="item">
+            <img src="images/afbeelding3.jpg" alt="carousel image 3">
+        </div>
+
+        <div class="item">
+            <img src="images/afbeelding4.jpg" alt="carousel image 4">
+        </div>
+    </div>
+
+    <div id='clicker'>
+      <h1>EXPAND</h1>
+    </div>
+
+    <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+        <span class="glyphicon glyphicon-chevron-left"></span>
+        <span class="sr-only">Previous</span>
+    </a>
+    <a class="right carousel-control" href="#myCarousel" data-slide="next">
+        <span class="glyphicon glyphicon-chevron-right"></span>
+        <span class="sr-only">Next</span>
+    </a>
+</div>
--- w24-25-templating/index.php
+++ w24-25-templating/index.php
@@ -9,10 +9,8 @@
 
 	<?php
 		include 'include/header.php';
+		include 'include/carousel.php';
 	?>
-	<div id='bottom-header'>
-		<h1>Headertext</h1>
-	</div>
 
 	<div id='card1' class='cards'>
 		<div class='card-header'>
@@ -24,6 +22,16 @@
 	</div>
 
 	<div id='card2' class='cards'>
+		<div class='card-header'>
+			<h1>card-header</h1>
+		</div>
+		<div class='card-content'>
+			<p>Now that we know who you are, I know who I am. I'm not a mistake! It all makes sense! In a comic, you know how you can tell who the arch-villain's going to be? He's the exact opposite of the hero. And most times they're friends, like you and me! I should've known way back when... You know why, David? Because of the kids. They called me Mr Glass. </p>
+		</div>
+	</div>
+
+
+	<div id='card3' class='cards'>
 		<div class='card-header'>
 			<h1>card-header</h1>
 		</div>
