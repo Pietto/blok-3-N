06408750da1eaa0d3b77c1e500d1105a5e974f18
pietto <Pieterjan.v.dijk@gmail.com>
2019-06-04 14:41:32 +0200

    |

--- w24-25-templating/contact.php
+++ w24-25-templating/contact.php
@@ -9,17 +9,37 @@
 	<?php
 		include 'include/header.php';
 	?>
+	<?php
+		if ($_SERVER["REQUEST_METHOD"] == "POST") {
+	        $name = strip_tags($_POST['name']);
+	        $email = strip_tags($_POST['email']);
+	        $message = strip_tags($_POST['message']);
+	        echo '<p style="color:white;">',
+	        'Naam: ',$name,
+	        '<br>Email: ',$email,
+	        '<br>Bericht: ',$message,
+	    	'</p>';
+	    }
+    ?>
+
+    <?php
+		// the message
+		$msg = "First line of text\nSecond line of text";
+
+		// use wordwrap() if lines are longer than 70 characters
+		$msg = wordwrap($msg,70);
+
+		// send email
+		mail("pieterjan.v.dijk@Gmail.com","My subject",$msg);
+	?>
 
 	<form method="POST">
-		<p>Naam:</p> <input type="text" name="name" id='name'>
-		<p>Email:</p> <input type="text" name="email" id='email'>
-		<p>Bericht:</p> <textarea name="message" rows="6" cols="25"></textarea><br />
+		<p>Naam:</p> <input type="text" name="name" id='name' value='<?php echo $name; ?>'>
+		<p>Email:</p> <input type="text" name="email" id='email' value='<?php echo $email; ?>'>
+		<p>Email:</p> <input type="text" name="email" id='email' value='<?php echo $email; ?>'>
+		<p>Bericht:</p> <textarea name="message" rows="6" cols="25"><?php echo $message; ?></textarea><br />
 		<input type="submit" value="Send" id='send'><input type="reset" value="Clear" id='reset'>
 	</form>
-
-	<?php	
-	
-	?>
 
 	<script src="customizes/js.js"></script>
 	<script>
