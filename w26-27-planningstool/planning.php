142bfdf094703492bc7e69bd531f6da03432e055
pietto <Pieterjan.v.dijk@gmail.com>
2019-06-17 14:36:45 +0200

    \

--- w26-27-planningstool/planning.php
+++ w26-27-planningstool/planning.php
@@ -33,7 +33,28 @@
 	</div>
 
 	<div id='content'>
-		
+		<?php
+			$servername = "localhost";
+			$username = "root";
+			$password = "mysql";
+			$dbname = 'Planningstool';
+		    try {
+		        $conn = new PDO("mysql:host=$servername;dbname=planningstool", $username, $password);
+		        // set the PDO error mode to exception
+		        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		    }
+		    catch(PDOException $e){
+		        echo $sql . "<br>" . $e->getMessage();
+		    }
+		    $sql = "SELECT id, game, time, teacher, players FROM plannedgames";
+		    $stmt = $conn->prepare($sql);
+		    $stmt->execute();
+
+		    while ($data = $stmt->fetch())
+		        echo "<div class='card'>"
+		    	.$data['game']
+		    	."</div>";
+		?>
 	</div>
 
 
